07f2cacfcbf168c13af5593cafc05f5572ab1f43
J-HM <als091212@naver.com>
2020-07-08 19:41:54 +0900

    2번문제 시작

---
+++ parenthesis_transformation.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+
+using std::string;
+
+int main()
+{
+  string input;
+  std::cin >> input;
+
+  if (input == "") return 0;
+
+  // string u = divide(input);
+  // string v = 
+  return 0;
+}
