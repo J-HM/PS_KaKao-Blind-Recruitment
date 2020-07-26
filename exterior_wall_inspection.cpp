448c51372637e074e181f8f7f715e666bb3fdf36
J-HM <als091212@naver.com>
2020-07-26 15:52:28 +0900

    Too difficult!

--- exterior_wall_inspection.cpp
+++ exterior_wall_inspection.cpp
@@ -1,50 +1,76 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 
 using namespace std;
 
+int size;
+vector<int> weaks;
+vector<int> dists;
 
-int getMinimumDistance(int size, int first_index, int second_index)
+
+int getWeaksDistance(int first_index, int second_index)
 {
-  int big_index, small_index;
-  if (first_index > second_index)
-  {
-    big_index = first_index;
-    small_index = second_index;
-  }
+  if (second_index > first_index)
+    return weaks[second_index] - weaks[first_index];
+  else if (second_index == first_index)
+    return 0;
   else
-  {
-    big_index = second_index;
-    small_index = first_index;
-  }
-  int distance = big_index - small_index;
-  if (distance < size / 2)
-    return distance;
-  else
-    return size - distance;
+    return size + weaks[second_index] - weaks[first_index];
 }
 
+// int inspection(int weak_index, int dist_index, int remaining_weaks_count)
+// {
+//   if (remaining_weaks_count == 0) 
+//     return 1;
+//   int insepction_size = dists.at(dist_index);
+//   int next_weak_index = weak_index + 1;
+//   while(next_weak_index < weaks.size() + weak_index)
+//   {
+//     next_weak_index++;
+//     int distance = getWeaksDistance(weak_index, next_weak_index);
+//     if (distance < insepction_size)
+//       break;
+//   }
+//   return 1 + inspection(next_weak_index, dist_index, remaining_weaks_count - 1);
+// }
 
-int solution(int n, vector<int> weak, vector<int> dist) {
+int inspection()
+{
   
-  for (int i = 0; i < weak.size(); i++)
-    for (int j = i + 1; j < weak.size(); j++)
-      cout << i << " and " << j << " distance : " << getMinimumDistance(n, weak[i], weak[j]) << endl;
-  int result = 0;
+  for (int i = 0; i < weaks.size(); i++)
+  {
+  
+    for (int j = i; j < weaks.size() + i; j++)
+    {
 
-  return result;
+    }
+
+  }
+}
+
+int solution(int input_size, vector<int> input_weaks, vector<int> input_dists)
+{
+  size = input_size;
+  weaks = input_weaks;
+  dists = input_dists;
+
+  sort(dists.begin(), dists.end(), greater<int>()); // Reverse dists
+  
+  int inspection_count = inspection(0, 0, weaks.size());
+  return inspection_count;
 }
 
 int main()
 {
-  int n;
-  vector<int> weak;
-  vector<int> dist;
-  n = 12;
-  weak = {1, 5, 6, 10};
-  dist = {1, 2, 3, 4};	
+  int size;
+  vector<int> weaks;
+  vector<int> dists;
+  size = 12;
+  weaks = {1, 5, 6, 10};
+  dists = {1, 2, 3, 4};	
 
-  auto result = solution(n, weak, dist);
+  auto result = solution(size, weaks, dists);
   cout << result << endl;
   return 0;
 }
